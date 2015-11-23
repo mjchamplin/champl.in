f2226309b107afbe533980120549479b27b77c0b
Michael Champlin <mjchamplin@gmail.com>
2015-11-23 12:04:23 -0500

    Cleaning up functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,25 +1,17 @@
-
-
 // remap jQuery to $
 (function($){
-
-
-
-
 	/* trigger when page is ready */
 	$(document).ready(function (){
-
         
         var instaLat = {};
         var instaLon = {};
         var instaSrc = {};
-        var instaTitle = {};
-        
+        var instaTitle = {};        
 				//Fallback coordinates (in case Instagram returns null)
         instaLat = 30.2921987;
         instaLon = -97.7355486;
         
-        
+        //Get latest cache.json file
         $.ajax({
           url: "loc/cache.json",
           async: false,
@@ -32,16 +24,17 @@
           }
         });
         
-        
+				//log latest IG title for testing
         console.log(instaTitle);
      
-        L.mapbox.map('mapbox', 'bobbysud.lff265id', { scrollWheelZoom: false, dragging: false, center: 
-[instaLat,instaLon], zoom: 12});
+				//Create Mapbox Map, center on IG lat/long
+        L.mapbox.map('mapbox', 'bobbysud.lff265id', { scrollWheelZoom: false, dragging: false, center: [instaLat,instaLon], zoom: 12});
         
+				//Find .ig and put IG image there (not used)
         $(".ig").fadeOut( 100 ).delay( 800 ).fadeIn( 400 ).append("<img class='instagram-image' src='" + instaSrc +"' />"); 
 
+				//Configures and initializes fullpage.js
         $('#fullpage').fullpage({
-            //verticalCentered: true,
             resize : true,
             sectionsColor : [],
             anchors:['slideOne', 'slideTwo', 'slideThree', 'slideFour', 'slideFive'],
@@ -59,12 +52,7 @@
             autoScrolling: true,
             scrollOverflow: false,
             css3: true,
-            //paddingTop: '3em',
-            //paddingBottom: '10px',
-            //normalScrollElements: '#element1, .element2',
-            //normalScrollElementTouchThreshold: 5,
             keyboardScrolling: true,
-            //touchSensitivity: 3,
             continuousVertical: false,
             animateAnchor: true,
             sectionSelector: '.section',
@@ -77,9 +65,6 @@
             afterResize: function(){},
             afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
             onSlideLeave: function(anchorLink, index, slideIndex, direction){}
-        }); 
-   
-        
-          
-	});
+        });                      
+		});
 })(window.jQuery);
